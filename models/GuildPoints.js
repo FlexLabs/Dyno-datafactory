ad8e08972bbfbe01443ba47aef059111326a58d3
Germano Eichenberg <germanoeich@gmail.com>
2017-10-25 13:29:49 -0200

    Add userId to guildpoints

--- models/GuildPoints.js
+++ models/GuildPoints.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 
 const guildPointsSchema = new Schema({
     guildId:     { type: String, required: true, index: true },
+    userId:      { type: String, required: true, index: true },
     guild:       { type: Object, required: true },
     user:        { type: Object, required: true },
     points:      { type: Number, required: true },
