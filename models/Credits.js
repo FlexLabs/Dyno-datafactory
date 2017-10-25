d88fadfb3b4474f8ffda3a28087350df954b6f32
Germano Eichenberg <germanoeich@gmail.com>
2017-10-25 09:07:09 -0200

    Credits/GuildPoints/XP schemas

---
+++ models/Credits.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const creditsSchema = new Schema({
+    userId:      { type: String, required: true, index: true },
+    user:        { type: Object, required: true },
+    credits:          { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Credits', creditsSchema);
---
+++ models/GuildPoints.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const guildPointsSchema = new Schema({
+    guildId:     { type: String, required: true, index: true },
+    guild:       { type: Object, required: true },
+    user:        { type: Object, required: true },
+    points:      { type: Number, required: true },
+});
+
+module.exports = mongoose.model('GuildPoints', guildPointsSchema);
