f0007502cf5dd69e20e561613d8515e63a42e632
MrJohnCoder <johnsmithcoder7@gmail.com>
2018-02-21 20:54:55 -0500

    add a model and changed another model

--- models/StreamAlert.js
+++ models/StreamAlert.js
@@ -8,7 +8,7 @@ const streamAlertSchema = new Schema({
 	handle: { type: String, required: true },
 	channel: { type: String, required: true },
 	guild: { type: String, required: true, index: true },
-	user: { type: Object, required: true },
+	webhook: { type: Object, required: true },
 	streaming: { type: Boolean, default: false },
 }, { strict: false });
 
