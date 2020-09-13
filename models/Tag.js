5f049cdad1e856705e439abd3bd720cd5eba372f
Brian Tanner <brian@tanner.io>
2020-09-13 19:01:31 -0400

    add category to tag schema

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,7 @@ const tagSchema = new Schema({
 	guild:       { type: String, required: true, index: true },
 	author:      { type: Object, required: true },
 	tag:         { type: String },
+	category:    { type: String },
 	content:     { type: String },
 	usage: 		 { type: Number, default: 0 },
 	createdAt:   { type: Date, default: Date.now },
